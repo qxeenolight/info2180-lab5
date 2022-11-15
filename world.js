a92a38a9303948d1e0cb78a351ac82218ee85ad7
Tiffany Parkinson <banana13.tp@gmail.com>
2022-11-14 23:36:22 -0500

    Added world.js

---
+++ world.js
@@ -0,0 +1,29 @@
+onload = function(){
+    //Variables
+    var request = new XMLHttpRequest();
+    var btn = document.querySelector("#lookup");
+    var input = document.querySelector("#country");
+    var output = document.querySelector("#result");
+    var url = "world.php?country=";
+
+    btn.addEventListener("click", e =>{
+        e.preventDefault();
+
+        //AJAX request using XMLHttpRequest
+        request.onreadystatechange = e =>{
+            if(request.readyState == 4){
+                if(request.status == 200){
+                    var answer = request.responseText;
+                    output.innerHTML = answer;
+                    console.log(":D");
+                }
+                else{
+                    output.innerHTML = "An error occured X_X";
+                    console.log("D:");
+                }
+            }
+        }
+        request.open("GET", url +  input.value);
+        request.send();
+    });
+}
