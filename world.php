0175534442eeffe542212234f41b56942bac3c17
Tiffany Parkinson <banana13.tp@gmail.com>
2022-11-14 23:37:54 -0500

    Updated to solve Excercise 3

--- world.php
+++ world.php
@@ -1,17 +1,34 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$output = "";
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($_SERVER["REQUEST_METHOD"] == "GET"){
+  if (isset($_GET['country']) && !empty($_GET['country']) && !isset($_GET['context']) ){
+    $filteredCountry = ucwords(trim(filter_var($_GET["country"], FILTER_SANITIZE_STRING)));
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$filteredCountry%' ");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $output = "<ul>";
+    foreach($results as $country){
+      $output .= "<li>".$country["name"]."<br>";
+    }
+    $output.= "</ul>";
+  }
 
+  else{
+    $output = "<ul>";
+    $stmt = $conn->query("SELECT * FROM countries ");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($results as $country){
+      $output .= "<li>".$country["name"]."<br>";
+    }
+    $output.= "</ul>";
+  }
+
+  echo $output;
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
