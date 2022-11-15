03a9f37093e73d2f3f3bcbfa0a41e6798047abec
Tiffany Parkinson <banana13.tp@gmail.com>
2022-11-15 00:24:04 -0500

    Updated to solve Excercise 5

--- world.js
+++ world.js
@@ -2,6 +2,7 @@ onload = function(){
     //Variables
     var request = new XMLHttpRequest();
     var btn = document.querySelector("#lookup");
+    var btn2 = document.querySelector("#citylookup");
     var input = document.querySelector("#country");
     var output = document.querySelector("#result");
     var url = "world.php?country=";
@@ -24,6 +25,27 @@ onload = function(){
             }
         }
         request.open("GET", url +  input.value);
+        request.send();
+    });
+
+    btn2.addEventListener("click", e =>{
+        e.preventDefault();
+
+        //AJAX request using XMLHttpRequest
+        request.onreadystatechange = e =>{
+            if(request.readyState == 4){
+                if(request.status == 200){
+                    var answer = request.responseText;
+                    output.innerHTML = answer;
+                    console.log(":D");
+                }
+                else{
+                    output.innerHTML = "An error occured X_X";
+                    console.log("D:");
+                }
+            }
+        }
+        request.open("GET", url +  input.value + "&context=cities");
         request.send();
     });
 }
--- world.php
+++ world.php
@@ -31,9 +31,48 @@ if ($_SERVER["REQUEST_METHOD"] == "GET"){
         </tbody>
     </table> <?php  
   }
-
-  else{
-    $output = "<ul>";
+  elseif(isset($_GET['country']) && !empty($_GET['country']) && isset($_GET['context']) && $_GET['context'] == "cities"){
+    $filteredCountry = ucwords(trim(filter_var($_GET['country'], FILTER_SANITIZE_STRING)));
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code  WHERE countries.name LIKE '%$filteredCountry%' ");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC); ?>
+    <table>
+        <tr> 
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
+        </tr>
+        <tbody> 
+            <?php foreach ($results as $city): ?>
+              <tr> 
+                  <td><?= $city['name']?></td>
+                  <td><?= $city['district']?></td>
+                  <td><?= $city['population']?></td>
+              </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table> <?php  
+  }
+  elseif((empty($_GET['country'])) && isset($_GET['context']) && $_GET['context'] == "cities"){
+    $stmt = $conn->query("SELECT * FROM cities ");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC); ?>
+    <table>
+        <tr> 
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
+        </tr>
+        <tbody> 
+            <?php foreach ($results as $city): ?>
+              <tr> 
+                  <td><?= $city['name']?></td>
+                  <td><?= $city['district']?></td>
+                  <td><?= $city['population']?></td>
+              </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table> <?php  
+  }
+  elseif((empty($_GET['country'])) || !isset($_GET['country']) && !isset($_GET['context'])){
     $stmt = $conn->query("SELECT * FROM countries ");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
     <table>
